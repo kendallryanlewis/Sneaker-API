cf2c0b3f92b3a0e6f3c2c61321a795d45708ce49
kendallryanlewis <kendall.ryan.lewis@gmail.com>
2023-04-06 00:59:31 -0500

    Changing layout
    Changing layout

--- README.md
+++ README.md
@@ -19,6 +19,8 @@ A StockX API, FlightClub API, Goat API, and Stadium Goods API all in one.
 
 Sneaks API is a sneaker API built using Node.JS, Express, and Got. The Sneaks API allows users to get essential sneaker content such as images, product links and even prices from resell sites while also collecting data and storing it within a database. This API mainly scrapes StockX for sneaker information and then asynchronously scrapes Stadium Goods, Goat, and Flight Club for additional sneaker information such as images and its respective resell price. This API outputs a sneaker object of the following variables:
 
+**UPDATE 1.2.3**: Updated and fixed the Goat API to reflect their API changes. Removed the web server (localhost:4000) the package creates.
+
   - Sneaker Name
   - Colorway
   - Description
@@ -35,11 +37,12 @@ Sneaks API is a sneaker API built using Node.JS, Express, and Got. The Sneaks AP
 
 I built this API so sneaker heads and developers are able to create sneaker based programs, trackers and websites without having to fumble with scrapping information on all 4 resell websites. Feel free to fork, edit and submit a pull request for this API for any changes or improvements. If you have any questions or issues regarding this feel free to create an issue and I will try to answer them as soon as I can.
 
-UPDATE: As per many requests, I updated this API to version 1.1 which removes the caching database from the API so no need to download and have MongoDB running for the API. If youd like to use the database version of the API, it is now a depreciated [branch](https://github.com/druv5319/Sneaks-API/tree/API-with-database) to this repository.
 ## Demo
-### Sneaks App - [Website](https://sneaks-app.com) | [Github](https://github.com/druv5319/sneaks-app)
-<img src="https://github.com/druv5319/Sneaks-API/blob/master/Screenshots/demo.gif" width=600 >
-  
+### Sneaks App - [Github](https://github.com/druv5319/sneaks-app)
+<img src="https://github.com/druv5319/Sneaks-API/blob/master/Screenshots/demo.gif" width=700 >
+
+### [Twilio Blog Tutorial](https://www.twilio.com/blog/build-price-tracker-twilio-programmable-sms-node): Build a Sneaker Price Tracker with Twilio Programmable SMS
+  <img src="https://github.com/druv5319/Sneaks-API/blob/master/Screenshots/euCdtWvMESTjo9_Smd_wRpoNvhk6XOr3n3QlnHYNRAHHU.width-1000_RWrysBn-2.png" width="350"> <img src="https://github.com/druv5319/Sneaks-API/blob/master/Screenshots/syHg2FzA1dcFjpp6XlqNq_ZRNDXJPcdWCJzGhBcU2PPXv.width-1000_VZzgt9G.png" width="350">
 
 
 ## Technologies Used
@@ -52,8 +55,8 @@ UPDATE: As per many requests, I updated this API to version 1.1 which removes th
 
   
 ## Installation
-To use this API you will need to have [node.js](https://nodejs.org/en/) installed and running.
-Once installed, use this line on the terminal within your project directory
+To use this API you will need to have [node.js](https://nodejs.org/en/) installed.
+Once installed, use this line on the terminal within your Node.js project directory
 ```
 npm install sneaks-api
 ```
@@ -62,13 +65,12 @@ and place this line at the top of your main file
 const SneaksAPI = require('sneaks-api');
 ```
 ## How to Use
-### Method #1: Using the SneaksAPI class
 ```js
 const SneaksAPI = require('sneaks-api');
 const sneaks = new SneaksAPI();
 
-//getProducts(keyword, callback) takes in a keyword and returns an array of products
-sneaks.getProducts("Yeezy Cinder", function(err, products){
+//getProducts(keyword, limit, callback) takes in a keyword and limit and returns a product array 
+sneaks.getProducts("Yeezy Cinder", 10, function(err, products){
     console.log(products)
 })
 
@@ -77,33 +79,11 @@ sneaks.getProducts("Yeezy Cinder", function(err, products){
 sneaks.getProductPrices("FY2903", function(err, product){
     console.log(product)
 })
-//getMostPopular(callback) returns an array of the current popular products curated by StockX
-sneaks.getMostPopular(function(err, products){
+//getMostPopular(limit, callback) takes in a limit and returns an array of the current popular products curated by StockX
+sneaks.getMostPopular(10, function(err, products){
     console.log(products)
 })
 ```
 [Console log](https://github.com/druv5319/Sneaks-API/blob/master/Screenshots/exampleSearchScreenshot%231.png) of sneaks.getProducts("Yeezy Cinder", ...)           
 [Console log](https://github.com/druv5319/Sneaks-API/blob/master/Screenshots/exampleSearchScreenshot%232.png) of sneaks.getProductPrices("FY2903", ...)
-
-### Method #2: Using localhost:3000
-Once your program starts with the sneaks-api module imported, a server should start and listen on port 3000
-
-<b>Routes:</b>
-
-This route takes in a keyword and returns an array of products (getProducts(keyword))
-```
-GET localhost:3000/search/:keyword
-```
-
-
-This route takes in a style ID and returns sneaker info including a price map and more images of the product (getProductprices(styleID))
-```
-GET localhost:3000/id/:styleID/prices
-```
-
-
-This route returns an array of the current popular products curated by StockX (getMostPopular())
-```
-GET localhost:3000/home
-```
 
--- controllers/sneaks.controllers.js
+++ controllers/sneaks.controllers.js
@@ -3,7 +3,6 @@ const stockXScraper = require('../scrapers/stockx-scraper');
 const flightClubScraper = require('../scrapers/flightclub-scraper');
 const goatScraper = require('../scrapers/goat-scraper');
 const stadiumGoodsScraper = require('../scrapers/stadiumgoods-scraper');
-const snkrsScraper = require('../scrapers/snkrs-scraper');
 
 module.exports = class Sneaks {
     /* findOne (shoeID, callback) {
@@ -18,177 +17,170 @@ module.exports = class Sneaks {
      });
    };*/
 
-    /*exports.create = async function (req, res) {
-      if (!req.params) {
-        return res.status(400).send({
-          message: "Shoe parameters can not be empty"
-        });
-      }
-      getProducts(req.params.shoe, function (error, products) {
-        if (error) {
+  /*exports.create = async function (req, res) {
+    if (!req.params) {
+      return res.status(400).send({
+        message: "Shoe parameters can not be empty"
+      });
+    }
+    getProducts(req.params.shoe, function (error, products) {
+      if (error) {
 
-          console.log(error)
-          res.send("Product Not Found");
-        } else {
-          res.json(products);
+        console.log(error)
+        res.send("Product Not Found");
+      } else {
+        res.json(products);
+      }
+    });
+  };*/
+  async getProducts(keyword, count = 40, callback) {
+
+    var productCounter = 0;
+    stockXScraper.getProductsAndInfo(keyword, count, function (error, products) {
+      if (error) {
+        callback(error, null)
+      }
+      products.forEach(function (shoe) {
+        var cbCounter = 0;
+        flightClubScraper.getLink(shoe, function () {
+          if (++cbCounter == 3) {
+            //if all shoes links have been parsed then return
+            if (productCounter++ + 1 == products.length) {
+              callback(null, products);
+            }
+
+          }
+        });
+
+        stadiumGoodsScraper.getLink(shoe, function () {
+          if (++cbCounter == 3) {
+            //if all shoes links have been parsed then return
+            if (productCounter++ + 1 == products.length) {
+              callback(null, products);
+            }
+
+          }
+        });
+
+        goatScraper.getLink(shoe, function () {
+          if (++cbCounter == 3) {
+            //if all shoes links have been parsed then return
+            if (productCounter++ + 1 == products.length) {
+              callback(null, products);
+            }
+
+          }
+        });
+      });
+
+    });
+
+
+  }
+
+  getProductPrices(shoeID, callback) {
+    const getPrices = (shoe) => {
+      var cbCounter = 0;
+      stockXScraper.getPrices(shoe, function () {
+        cbCounter++;
+        if (cbCounter == 5) {
+          callback(null, shoe)
         }
       });
-    };*/
-    async getProducts(keyword, count = 40, callback) {
-        var productCounter = 0;
-        stockXScraper.getProductsAndInfo(keyword, count, function(error, products) {
-            if (error) {
-                callback(error, null)
-            }
-            products.forEach(function(shoe) {
-                var cbCounter = 0;
-                flightClubScraper.getLink(shoe, function() {
-                    if (++cbCounter == 3) {
-                        //if all shoes links have been parsed then return
-                        if (productCounter++ + 1 == products.length) {
-                            callback(null, products);
-                        }
-                    }
-                });
-                stadiumGoodsScraper.getLink(shoe, function() {
-                    if (++cbCounter == 3) {
-                        //if all shoes links have been parsed then return
-                        if (productCounter++ + 1 == products.length) {
-                            callback(null, products);
-                        }
-                    }
-                });
-                goatScraper.getLink(shoe, function() {
-                    if (++cbCounter == 3) {
-                        //if all shoes links have been parsed then return
-                        if (productCounter++ + 1 == products.length) {
-                            callback(null, products);
-                        }
-                    }
-                });
-            });
-        });
+      stadiumGoodsScraper.getPrices(shoe, function () {
+        cbCounter++;
+        if (cbCounter == 5) {
+          callback(null, shoe)
+        }
+
+      });
+      flightClubScraper.getPrices(shoe, function () {
+        cbCounter++;
+        if (cbCounter == 5) {
+          callback(null, shoe)
+        }
+
+      });
+      goatScraper.getPrices(shoe, function () {
+        cbCounter++;
+        if (cbCounter == 5) {
+          callback(null, shoe)
+        }
+      });
+      goatScraper.getPictures(shoe, function () {
+        cbCounter++;
+        if (cbCounter == 5) {
+          callback(null, shoe)
+        }
+      });
     }
 
-    getProductPrices(shoeID, count = 1, callback) {
-        const getPrices = (shoe) => {
-            var cbCounter = 0;
-            stockXScraper.getPrices(shoe, function() {
-                cbCounter++;
-                if (cbCounter == 5) {
-                    callback(null, shoe)
-                }
-            });
-            stadiumGoodsScraper.getPrices(shoe, function() {
-                cbCounter++;
-                if (cbCounter == 5) {
-                    callback(null, shoe)
-                }
+    getProducts(shoeID, 1, function (error, products) {
+      if (error || products[0].styleID.toLowerCase() != shoeID.toLowerCase()) {
+        console.log(new Error("No Products Found"));
+        callback(new Error("No Products Found"), null);
+        return;
+      }
+      getPrices(products[0]);
+    });
+  };
 
-            });
-            flightClubScraper.getPrices(shoe, function() {
-                cbCounter++;
-                if (cbCounter == 5) {
-                    callback(null, shoe)
-                }
+  /*findAll(callback) {
+    Sneaker.find()
+      .then(sneaks => {
+        callback(null, sneaks);
+      }).catch(err => {
+        callback(err, null)
+      });
+  };*/
 
-            });
-            goatScraper.getPrices(shoe, function() {
-                cbCounter++;
-                if (cbCounter == 5) {
-                    callback(null, shoe)
-                }
-            });
-            goatScraper.getPictures(shoe, function() {
-                cbCounter++;
-                if (cbCounter == 5) {
-                    callback(null, shoe)
-                }
-            });
-        }
-
-        getProducts(shoeID, count, function(error, products) {
-            if (error) {
-                console.log(new Error("No Products Found"));
-                callback(error, null);
-                return;
-            }
-            getPrices(products[0]);
-        });
-    };
-
-    /*findAll(callback) {
-      Sneaker.find()
-        .then(sneaks => {
-          callback(null, sneaks);
-        }).catch(err => {
-          callback(err, null)
-        });
-    };*/
-
-    getMostPopular(count, callback) {
-        getProducts("", count, function(error, products) {
-            if (error) {
-                callback(error, null);
-            } else {
-                callback(null, products)
-            }
-        });
-    };
-    getSnkrs(count, callback) {
-        getSnkrs("", count, function(error, products) {
-            if (error) {
-                callback(error, null);
-            } else {
-                callback(null, products)
-            }
-        });
-    };
+  getMostPopular(count, callback) {
+    getProducts("", count, function (error, products) {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, products)
+      }
+    });
+  };
 }
 
 
-var getProducts = function(keyword, count = 40, callback) {
-    var productCounter = 0;
-    stockXScraper.getProductsAndInfo(keyword, count, function(error, products) {
-        if (error) {
-            callback(error, null)
+var getProducts = function (keyword, count = 40, callback) {
+  var productCounter = 0;
+  stockXScraper.getProductsAndInfo(keyword, count, function (error, products) {
+    if (error) {
+      callback(error, null)
+    }
+    products.forEach(function (shoe) {
+      var cbCounter = 0;
+      flightClubScraper.getLink(shoe, function () {
+        if (++cbCounter == 3) {
+          //if all shoes links have been parsed then return
+          if (productCounter++ + 1 == products.length) {
+            callback(null, products);
+          }
         }
-        products.forEach(function(shoe) {
-            var cbCounter = 0;
-            flightClubScraper.getLink(shoe, function() {
-                if (++cbCounter == 3) {
-                    //if all shoes links have been parsed then return
-                    if (productCounter++ + 1 == products.length) {
-                        callback(null, products);
-                    }
-                }
-            });
+      });
 
-            stadiumGoodsScraper.getLink(shoe, function() {
-                if (++cbCounter == 3) {
-                    //if all shoes links have been parsed then return
-                    if (productCounter++ + 1 == products.length) {
-                        callback(null, products);
-                    }
-                }
-            });
+      stadiumGoodsScraper.getLink(shoe, function () {
+        if (++cbCounter == 3) {
+          //if all shoes links have been parsed then return
+          if (productCounter++ + 1 == products.length) {
+            callback(null, products);
+          }
 
-            goatScraper.getLink(shoe, function() {
-                if (++cbCounter == 3) {
-                    //if all shoes links have been parsed then return
-                    if (productCounter++ + 1 == products.length) {
-                        callback(null, products);
-                    }
-                }
-            });
-        });
-    });
-}
-var getSnkrs = function(keyword, count = 40, callback) {
-    var productCounter = 0;
-    snkrsScraper.getProductsAndInfo(keyword, count, function(error, products) {
-        if (error) {
-            callback(error, null)
         }
+      });
+
+      goatScraper.getLink(shoe, function () {
+        if (++cbCounter == 3) {
+          //if all shoes links have been parsed then return
+          if (productCounter++ + 1 == products.length) {
+            callback(null, products);
+          }
+        }
+      });
     });
+  });
 }
