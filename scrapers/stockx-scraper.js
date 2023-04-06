bf05e8349f8ce5c83bd2174af74817660bc98e39
kendallryanlewis <kendall.ryan.lewis@gmail.com>
2023-04-06 01:32:51 -0500

    v4
    v4

--- scrapers/stockx-scraper.js
+++ scrapers/stockx-scraper.js
@@ -3,8 +3,7 @@ const Sneaker = require('../models/Sneaker');
 
 
 module.exports = {
-    getProductsAndInfo: async function (key, count, callback) {
-      
+    getProductsAndInfo: async function(key, count, callback) {
         try {
             const response = await got.post('https://xw7sbct9v6-1.algolianet.com/1/indexes/products/query?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%203.32.1&x-algolia-application-id=XW7SBCT9V6&x-algolia-api-key=6b5e76b49705eb9f51a06d3c82f7acee', {
                 headers: {
@@ -19,8 +18,8 @@ module.exports = {
                 body: `{"params":"query=${key}&facets=*&filters=&hitsPerPage=${count}"}`,
                 http2: true
             });
-           
-            
+
+
             var json = JSON.parse(response.body);
             var products = [];
             var numOfShoes = json.hits.length;
@@ -46,26 +45,25 @@ module.exports = {
                         stockX: 'https://stockx.com/' + json.hits[i].url
                     }
                 });
-                if(json.hits[i].lowest_ask){
+                if (json.hits[i].lowest_ask) {
                     shoe.lowestResellPrice.stockX = json.hits[i].lowest_ask;
                 }
                 products.push(shoe)
             }
-          
+
             if (products.length == 0 || numOfShoes == 0) {
-                callback(new Error('Product Not Found'), null);
-            } 
-            else {
+                callback(new Error('Product Info Not Found'), null);
+            } else {
                 callback(null, products);
             }
         } catch (error) {
-            let err = new Error("Could not connect to StockX while searching '", key, "' Error: ", error)
+            let err = new Error("Could not connect to StockX while searching '", shoe.styleID, "' Error: ", error)
             console.log(err);
             callback(err, products)
         }
     },
 
-    getPrices: async function (shoe, callback) {
+    getPrices: async function(shoe, callback) {
         let priceMap = {}
         try {
             const response = await got('https://stockx.com/api/products/' + shoe.urlKey + '?includes=market', {
@@ -75,16 +73,16 @@ module.exports = {
                 http2: true
             });
             let json = JSON.parse(response.body);
-            Object.keys(json.Product.children).forEach(function (key) {
+            Object.keys(json.Product.children).forEach(function(key) {
                 if (json.Product.children[key].market.lowestAsk == 0) return;
                 //if size is in womens, then remove "W"
                 var size = json.Product.children[key].shoeSize
-                if(size[size.length-1] == 'W'){
+                if (size[size.length - 1] == 'W') {
                     size = size.substring(0, size.length - 1);
-                    
+
                 }
                 priceMap[size] = json.Product.children[key].market.lowestAsk;
-                
+
             });
             shoe.resellPrices.stockX = priceMap;
             callback();
