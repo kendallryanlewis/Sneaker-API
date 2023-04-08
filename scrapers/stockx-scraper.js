0e71bc0e607c0b09817115617816f31d76bf22f0
kendallryanlewis <kendall.ryan.lewis@gmail.com>
2023-04-08 02:24:32 -0500

    v.6

--- scrapers/stockx-scraper.js
+++ scrapers/stockx-scraper.js
@@ -3,8 +3,7 @@ const Sneaker = require('../models/Sneaker');
 
 
 module.exports = {
-    getProductsAndInfo: async function (key, count, callback) {
-      
+    getProductsAndInfo: async function(key, count, callback) {
         try {
             const response = await got.post('https://xw7sbct9v6-1.algolianet.com/1/indexes/products/query?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%203.32.1&x-algolia-application-id=XW7SBCT9V6&x-algolia-api-key=6b5e76b49705eb9f51a06d3c82f7acee', {
                 headers: {
@@ -19,11 +18,14 @@ module.exports = {
                 body: `{"params":"query=${key}&facets=*&filters=&hitsPerPage=${count}"}`,
                 http2: true
             });
-           
-            
+
+
             var json = JSON.parse(response.body);
+            console.log("Showing get products and info stockx", json);
             var products = [];
             var numOfShoes = json.hits.length;
+
+            console.log("Showing number of shoes stockx", numOfShoes);
 
             for (var i = 0; i < json.hits.length; i++) {
                 if (!json.hits[i].style_id || (json.hits[i].style_id).indexOf(' ') >= 0) {
@@ -46,16 +48,17 @@ module.exports = {
                         stockX: 'https://stockx.com/' + json.hits[i].url
                     }
                 });
-                if(json.hits[i].lowest_ask){
+                if (json.hits[i].lowest_ask) {
                     shoe.lowestResellPrice.stockX = json.hits[i].lowest_ask;
                 }
                 products.push(shoe)
+
+                console.log("Showing product stockx", products);
             }
-          
+
             if (products.length == 0 || numOfShoes == 0) {
                 callback(new Error('Product Not Found'), null);
-            } 
-            else {
+            } else {
                 callback(null, products);
             }
         } catch (error) {
@@ -65,7 +68,7 @@ module.exports = {
         }
     },
 
-    getPrices: async function (shoe, callback) {
+    getPrices: async function(shoe, callback) {
         let priceMap = {}
         try {
             const response = await got('https://stockx.com/api/products/' + shoe.urlKey + '?includes=market', {
@@ -75,16 +78,17 @@ module.exports = {
                 http2: true
             });
             let json = JSON.parse(response.body);
-            Object.keys(json.Product.children).forEach(function (key) {
+            console.log("Showing get prices stockx", json);
+            Object.keys(json.Product.children).forEach(function(key) {
                 if (json.Product.children[key].market.lowestAsk == 0) return;
                 //if size is in womens, then remove "W"
                 var size = json.Product.children[key].shoeSize
-                if(size[size.length-1] == 'W'){
+                if (size[size.length - 1] == 'W') {
                     size = size.substring(0, size.length - 1);
-                    
+
                 }
                 priceMap[size] = json.Product.children[key].market.lowestAsk;
-                
+                console.log("Showing get prices stockx", json);
             });
             shoe.resellPrices.stockX = priceMap;
             callback();
