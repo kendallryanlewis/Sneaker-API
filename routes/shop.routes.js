d49bc12bf1e72e8723574121ad9fc9246cc52744
kendallryanlewis <kendall.ryan.lewis@gmail.com>
2023-07-14 01:53:21 -0400

    Cleaning up

--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -36,28 +36,6 @@ module.exports = (app) => {
                     })
                     res.send(releases)
                 }).catch(err => console.log(err))
-
-
-            /*
-                const city = req.params.city;
-                    const url = `https://www.yellowpages.com/search?search_terms=sneaker+stores&geo_location_terms=${city}`;
-
-                    const response = await axios.get(url);
-                    const $ = cheerio.load(response.data);
-
-
-                    const stores = [];
-
-                    $('.v-card').each((_, element) => {
-                        const name = $(element).find('.business-name').text().trim();
-                        const address = $(element).find('.adr').text().trim();
-                        const phone = $(element).find('.phones').text().trim();
-
-                        stores.push({ name, address, phone });
-                    });
-
-                    res.json(stores);
-            */
         } catch (error) {
             res.status(500).json({ error: 'An error occurred while fetching stores.' });
             console.error(error);
